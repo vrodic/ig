4ba4cfd621ea4742b324d33824f43128302e5aae
Vedran Rodic <vrodic@gmail.com>
2021-10-27 16:56:03 +0200

    fix hassio import

--- adapters/import_hassio.py
+++ adapters/import_hassio.py
@@ -1,6 +1,9 @@
 
 import urllib.request
 import json
+from datetime import datetime
+from dateutil import tz
+from dateutil import parser
 
 req = urllib.request.Request('http://localhost:8123/api/states')
 req.add_header('Content-Type', 'application/json')
@@ -9,6 +12,10 @@ req.add_header('Authorization','Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJp
 #jsondata = json.dumps(data)
 #jsondataasbytes = jsondata.encode('utf-8')
 response = urllib.request.urlopen(req)
+
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
+
 
 sensors = {
     'sensor.a4c1389591fd_humidity': {'location_id': 7, 'source_type': 'mith2-bedroom', 'type': 'humidity'},
@@ -28,11 +35,20 @@ json_r = json.loads(response.read().decode(response.info().get_param('charset') 
 post_items = []
 for item in json_r:
     if item['entity_id'] in sensors:
+        
+        utc = parser.parse(item['last_changed'])
+
+        # Tell the datetime object that it's in UTC time zone since 
+        # datetime objects are 'naive' by default
+        utc = utc.replace(tzinfo=from_zone)
+
+        # Convert time zone
+        local = utc.astimezone(to_zone)
         post_items.append( {
             "location_id": sensors[item['entity_id']]['location_id'],
             "source_type": sensors[item['entity_id']]['source_type'],
             "type": sensors[item['entity_id']]['type'],
-            "logged_at": item['last_changed'],
+            "logged_at": local.isoformat(),
             'value': item['state']
         })
 
